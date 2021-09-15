e5e42cf3ea62add2c1bc01c1b5fa06c80ecda3d4
Anzaiyasuto <al18011@shibaura-it.ac.jp>
2021-09-15 17:54:54 +0900

    0915v5

--- server.py
+++ server.py
@@ -1,30 +1,21 @@
 from flask import Flask, request, render_template
-import math
 app = Flask(__name__)
 file_path = "./sensor_data.csv"
 port_num = 18011
-flag = ["vacant", "occupied"] #flag = 0 -> vacant | flag = 1 -> occupied 
-beforeLux = 0.0
+flag = 0 #flag = 0 -> vacant | flag = 1 -> occupied 
+
 @app.route('/', methods=['GET'])
 def get_html():
-    flag=0
     return render_template('./index.html')
 
 @app.route('/lux', methods=['POST'])
 def update_lux():
     time = request.form["time"]
     lux = request.form["lux"]
-    lux[0] = float(lux[0])
-    
-    if (math.fabs(lux[0]-beforeLux) > 20)  and (math.fabs(lux[0]-beforeLux) < 800):
-        state = flag[1]
-    else:
-        state = flag[0]
 
-    beforeLux = lux[0]
     try:
         f = open(file_path, 'w')
-        f.write(time + "," + state)
+        f.write(time + "," + lux)
         return "succeeded to write"
     except Exception as e:
         print(e)
